83082b7519662d3505e68de10c20c7c764ed0170
Swap76 <swapnil.satish17@siesgst.ac.in>
2018-11-12 20:24:00 +0530

    Added getCommit function in Call Back Hell Problem

--- CallbackHellProblem.js
+++ CallbackHellProblem.js
@@ -4,6 +4,9 @@ const user = getUser(1,(user) => {
     getRepositories(user.gitHubUserName,(repo) => {
         console.log(repo.username);
         console.log('Repo',repo);
+        getCommits(repo,(commit) =>{
+            console.log(commit);
+        });
     })
 });
 
@@ -20,5 +23,12 @@ function getUser(id, callback){
 function getRepositories(username, callback){
     setTimeout(()=> {
         callback({username: username,repository: ['repo1','repo2','repo3']});
+    },2000);
+}
+
+function getCommits(repo, callback){
+    setTimeout(() => {
+        console.log('Calling GitHub API....');
+        callback(['commit']);
     },2000);
 }
