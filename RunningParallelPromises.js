4d787646de954b93fb0bb12d2285ee5044f33278
Swap76 <swapnil.satish17@siesgst.ac.in>
2018-11-12 21:16:28 +0530

    Added Parallel Promises

---
+++ RunningParallelPromises.js
@@ -0,0 +1,18 @@
+
+const p1 = new Promise ((resolve,reject) =>{
+    setTimeout(() =>{
+        console.log('Async operation 1.....');
+        resolve(1);
+    },2000);
+});
+
+const p2 = new Promise ((resolve,reject) =>{
+    setTimeout(() =>{
+        console.log('Async operation 2.....');
+        resolve(2);
+    },2000);
+});
+
+Promise.all([p1, p2])
+  .then(result => console.log(result))
+  .catch(err => console.log('Error', err.message));
